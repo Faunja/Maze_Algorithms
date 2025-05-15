1a2049c130869fe5bdb4aab7b52ecc92f9ff48df
Faunja <129631000+Faunja@users.noreply.github.com>
2025-05-15 18:14:26 -0400

    V0.3.0
    Added and now displayed the randomized Kruskal's algorithm.

---
+++ Maze_Algorithms/define_randomkruskal.py
@@ -0,0 +1,91 @@
+import random
+
+class define_Randomkruskal:
+	def check_closedBox(self):
+		for row in self.numbers:
+			for column in row:
+				if column != 0:
+					return True
+		return False
+
+	def replace_numbers(self, firstNumber, secondNumber):
+		if firstNumber < secondNumber:
+			replacement = firstNumber
+			replace = secondNumber
+		else:
+			replacement = secondNumber
+			replace = firstNumber
+		for row in range(self.mazeSize):
+			for column in range(self.mazeSize):
+				if self.numbers[row][column] == replace:
+					self.numbers[row][column] = replacement
+	
+	def create_maze(self):
+		closedBox = self.check_closedBox()
+		self.position = [random.randrange(self.mazeSize), random.randrange(self.mazeSize)]
+
+		if closedBox:
+			canMove = False
+			cantMove = [False, False, False, False]
+			
+			while canMove == False:
+				movement = random.randint(0, 4)
+
+				if movement == 0 and not cantMove[0]:
+					cantMove[0] = self.position[1] + 1 >= self.mazeSize
+					if not cantMove[0]:
+						cantMove[0] = self.numbers[self.position[1]][self.position[0]] == self.numbers[self.position[1] + 1][self.position[0]]
+					if not cantMove[0]:
+						self.maze[self.position[1]][self.position[0]][0] = 0
+						self.maze[self.position[1] + 1][self.position[0]][1] = 0
+						self.replace_numbers(self.numbers[self.position[1]][self.position[0]], self.numbers[self.position[1] + 1][self.position[0]])
+						canMove = True
+				
+				if movement == 1 and not cantMove[1]:
+					cantMove[1] = self.position[1] - 1 < 0
+					if not cantMove[1]:
+						cantMove[1] = self.numbers[self.position[1]][self.position[0]] == self.numbers[self.position[1] - 1][self.position[0]]
+					if not cantMove[1]:
+						self.maze[self.position[1]][self.position[0]][1] = 0
+						self.maze[self.position[1] - 1][self.position[0]][0] = 0
+						self.replace_numbers(self.numbers[self.position[1]][self.position[0]], self.numbers[self.position[1] - 1][self.position[0]])
+						canMove = True
+				
+				if movement == 2 and not cantMove[2]:
+					cantMove[2] = self.position[0] - 1 < 0
+					if not cantMove[2]:
+						cantMove[2] = self.numbers[self.position[1]][self.position[0]] == self.numbers[self.position[1]][self.position[0] - 1]
+					if not cantMove[2]:
+						self.maze[self.position[1]][self.position[0]][2] = 0
+						self.maze[self.position[1]][self.position[0] - 1][3] = 0
+						self.replace_numbers(self.numbers[self.position[1]][self.position[0]], self.numbers[self.position[1]][self.position[0] - 1])
+						canMove = True
+				
+				if movement == 3 and not cantMove[3]:
+					cantMove[3] = self.position[0] + 1 >= self.mazeSize
+					if not cantMove[3]:
+						cantMove[3] = self.numbers[self.position[1]][self.position[0]] == self.numbers[self.position[1]][self.position[0] + 1]
+					if not cantMove[3]:
+						self.maze[self.position[1]][self.position[0]][3] = 0
+						self.maze[self.position[1]][self.position[0] + 1][2] = 0
+						self.replace_numbers(self.numbers[self.position[1]][self.position[0]], self.numbers[self.position[1]][self.position[0] + 1])
+						canMove = True
+				
+				if cantMove[0] and cantMove[1] and cantMove[2] and cantMove[3]:
+					break
+
+	def __init__(self, mazeSize):
+		self.mazeSize = mazeSize
+		
+		self.maze = []
+		self.numbers = []
+		for row in range(self.mazeSize):
+			self.maze.append([])
+			self.numbers.append([])
+			for column in range(self.mazeSize):
+				self.maze[row].append([1, 1, 1, 1])
+				self.numbers[row].append(row * self.mazeSize + column)
+		self.position = [0, 0]
+		
+		self.create_maze()
+
--- define_maze.py
+++ define_maze.py
@@ -1,23 +1,28 @@
-import pygame, random, copy
-from pygame.locals import *
+import time
 from User.define_user import User
 from Maze_Algorithms.define_backtracker import define_Backtracker
 from Maze_Algorithms.define_huntkill import define_Huntkill
+from Maze_Algorithms.define_randomkruskal import define_Randomkruskal
 
 class define_Maze:
 	def __init__(self):
-		self.Maze = define_Huntkill(5)
+		self.Maze = define_Randomkruskal(5)
 		self.timePassed = 0
+		self.sleepTime = 2
+		self.sleptTime = time.time()
 		self.mazeTimes = []
 
 	def update_maze(self):
 		if self.Maze.check_closedBox():
 			self.Maze.create_maze()
 			self.timePassed += 1
+			self.sleptTime = time.time()
+			return
+		if self.sleepTime > time.time() - self.sleptTime:
 			return
 		self.mazeTimes.append([self.Maze.mazeSize, self.timePassed])
 		print("Size {}, time: {}".format(self.Maze.mazeSize, self.timePassed))
-		self.Maze = define_Huntkill(self.Maze.mazeSize + 5)
+		self.Maze = define_Randomkruskal(self.Maze.mazeSize + 5)
 		self.startTime = 0
 
 Maze = define_Maze()
